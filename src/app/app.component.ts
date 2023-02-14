dec5c218eb7a74d0aad5b370683fde52dd0cfa61
ramanarv66 <52023593+ramanarv66@users.noreply.github.com>
2023-02-14 18:12:57 +0530

    EVegStore Angular With Material and Bootstrap
    EVegStore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'evegetable-store';
+  addedItem: boolean = false;
+
+  constructor(public dialog: MatDialog) {
+
+  }
+  count: number = 0;
+  showModal: boolean = false;
+  vegList = [];
+  addItems() {
+    this.addedItem = true;
+    this.count = this.count + 1;
+  }
+  removeItems() {
+    if (this.count <= 0) {
+      this.showModal = true;
+      this.count = 0;
+      this.dialog.open(DialogComponent);
+    } else {
+   
+      this.count = this.count - 1;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+
+  declarations: [AppComponent, DialogComponent],
+  entryComponents: [DialogComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, MatDialogModule, MatButtonModule, MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
